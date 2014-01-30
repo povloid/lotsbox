417228a8ded3276db97fbcbcdbf3127798234bd0
povloid <t34box@gmail.com>
2014-01-30 20:00:09 +0600

    Добавлена поддержка работы с базой данных

--- project.clj
+++ project.clj
@@ -3,4 +3,30 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
+
+                 [enlive "1.1.5"]
+                 [hiccup "1.0.4"]
+                 
+                 [overtone/at-at "1.2.0"]
+
+                 [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                 [korma "0.3.0-RC6"]]
+
+  :plugins [[lein-ring "0.8.10"]]
+
+  :ring {:handler lotsbox.core/app
+         :init lotsbox.core/init
+         :auto-reload? true
+         :auto-refresh? true
+         :nrepl {:start? true}}
+
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]
+                        [ring-serve "0.1.2"]]}})
+
+
--- src/lotsbox/core.clj
+++ src/lotsbox/core.clj
@@ -1,6 +1,99 @@
-(ns lotsbox.core)
+(ns lotsbox.core
+  (:use compojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  (:use hiccup.core)
+  (:use hiccup.page)
+  (:use hiccup.form)
+  (:use hiccup.element)
+
+  (:use korma.db)
+  (:use korma.core)
+
+  (:require [clojure.java.io :as io]
+            [ring.middleware.reload :refer (wrap-reload)] ;; reload temlates
+            
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+
+            ;;[net.cgrand.enlive-html :as h]
+
+            [overtone.at-at :as at]
+            )
+  )
+
+
+
+;; DATA BASE ---------------------------------------------------------------------------------------
+
+(def pg-lotsbox (postgres {:host "localhost" 
+                   :port 5433 
+                   :db "lotsbox" 
+                   :make-pool? true 
+                   :user "lotsbox" 
+                   :password "lotsbox"}))
+
+
+(defdb korma-db pg-lotsbox)
+
+
+(declare lots)
+
+(defentity lots
+  (table :lots)
+  (pk :id))
+
+
+
+
+;; PARSER ------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+;; INIT --------------------------------------------------------------------------------------------
+
+(defn init []
+  (println "INIT SYSTEM")
+
+  (print "STARTING SCHEDULLER -> ")
+
+  (print "[ok]")
+
+  )
+
+
+
+
+;; WEB SERVER --------------------------------------------------------------------------------------
+
+(defroutes app-routes
+  (GET "/" [] "Hello World!")
+  ;;(GET "/" [] (main-page))
+
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(defn spec-middle 
+  "Дополнительняа промежуточная функция"
+  [handler]
+  (fn [request]
+    #_(println request)
+    #_(use 'politrend.handler :reload) ;; needs for enlive
+
+    (handler request)))
+
+
+
+(def site
+  (-> app-routes spec-middle))
+      
+(def app
+  (handler/site site))
+
+
+
